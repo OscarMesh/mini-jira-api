018bfa9ca34b66110b75e9a95884d5ddbb4ec0ee
Oscar Okereke <okerekeifeanyi65@gmail.com>
2025-05-18 14:17:10 +0100

    feat: cors

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,6 @@ import { AppModule } from './app.module';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { EnvironmentSchemaType } from './@types';
 import { ConfigService } from '@nestjs/config';
-import { allowedHeaders } from './config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -18,9 +17,9 @@ async function bootstrap() {
   SwaggerModule.setup('docs', app, document);
 
   app.enableCors({
-    origin: allowedHeaders(),
+    origin: '*',
     allowedHeaders:
-      'X-Requested-With, X-HTTP-Method-Override, x-api-key, x-referer-key, Content-Type, Accept, Observe, Authorization, Access-Control-Allow-Origin',
+      'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept, Observe, Authorization, Access-Control-Allow-Origin',
     methods: 'GET,PUT,POST,PATCH,DELETE,UPDATE,OPTIONS',
     credentials: true,
     optionsSuccessStatus: 200,
